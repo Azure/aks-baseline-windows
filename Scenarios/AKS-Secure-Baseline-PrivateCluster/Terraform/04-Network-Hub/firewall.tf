1295f7e91de37a8f334b21d09136748e3bac2d96
Ashish Sharma <106212685+ashis303@users.noreply.github.com>
2022-10-17 10:04:47 -0500

    Update firewall.tf
    changes

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/04-Network-Hub/firewall.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/04-Network-Hub/firewall.tf
@@ -31,4 +31,5 @@ module "firewall_rules_aks" {
 
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  firewallName        = azurerm_firewall.firewall.name
 }
