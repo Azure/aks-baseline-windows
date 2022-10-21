9241d6eaad8a8d4db9bb02db5f3e028f9b0ab6af
David Cintron <dacintro@microsoft.com>
2022-10-21 15:35:42 -0500

    Remove provider variables.

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/provider.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/provider.tf
@@ -21,13 +21,9 @@ terraform {
 
 provider "azurerm" {
     features {}
-    subscription_id = var.subscription_id #"82e70289-bf40-45f9-8476-eab93d2031f4"
-    tenant_id = var.tenant_id #"449fbe1d-9c99-4509-9014-4fd5cf25b014"
-    client_id  = var.client_id #"f5174545-d5ae-49ab-be0a-48eed78a9c5c"
-    client_secret = var.client_secret #"F8U8~bmRCb4Lu7l.25r6ETc928ZPzBaqzw"
 
 }
 
 provider "azuread" {
-  tenant_id = var.tenant_id #"449fbe1d-9c99-4509-9014-4fd5cf25b014"
+  
 }
