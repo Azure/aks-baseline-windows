b5024a628cd846eef49c4d05e1d2e936fd05a024
David Cintron <dacintro@microsoft.com>
2022-10-21 15:48:02 -0500

    Add backend config variables to variables.tf

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/variables.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/variables.tf
@@ -9,3 +9,15 @@ variable "aks_admin_group" {
 variable "aks_user_group" {
     default = "AKS App Dev Team"
 }
+
+variable "resource_group_name" {
+  
+}
+
+variable "storage_account_name" {
+  
+}
+
+variable "container_name" {
+  
+}
