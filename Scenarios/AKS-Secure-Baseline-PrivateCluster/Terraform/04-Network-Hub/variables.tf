8fb6bd3649d65293fee97890345e8267abf0bdfe
David Cintron <dacintro@microsoft.com>
2022-10-22 09:27:44 -0500

    Remove variables and clean up provider.tf

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/04-Network-Hub/variables.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/04-Network-Hub/variables.tf
@@ -34,27 +34,6 @@ variable "admin_username" {
   default = "sysadmin"
 }
 
-variable "subscription_id" {
-  description = "Azure subscription Id."
-  default = null
-}
-
-variable "tenant_id" {
-  description = "Azure tenant Id."
-   default = null
-}
-
-variable "client_id" {
-description = "Azure service principal application Id"
-default = null
-}
-
-variable "client_secret" {
-  description = "Azure service principal application Secret"
-  default = null
-}
-
-variable "access_key" {}
 variable "resource_group_name" {}
 variable "storage_account_name" {}
 variable "container_name" {}
