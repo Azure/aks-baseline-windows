743402b1cb7793e040421828f42f79ea17fb1c97
David Cintron <dacintro@microsoft.com>
2022-10-27 14:32:16 -0500

    remove ARM variables from provider.tf

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/provider.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/provider.tf
@@ -21,12 +21,9 @@ terraform {
 
 provider "azurerm" {
     features {}
-    subscription_id = var.subscription_id 
-    tenant_id = var.tenant_id 
-    client_id  = var.client_id 
-    client_secret = var.client_secret 
+    
 }
 
 provider "azuread" {
-  tenant_id = var.tenant_id 
+   
 }
