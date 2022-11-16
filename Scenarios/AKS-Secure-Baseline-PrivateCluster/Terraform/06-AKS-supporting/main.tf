e18c20a5e6f7dcdc4c5d9d4dbf0b7dd23aee16d0
David Cintron <dacintro@microsoft.com>
2022-11-15 22:00:07 -0600

    add remote state  exsisting hub

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/06-AKS-supporting/main.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/06-AKS-supporting/main.tf
@@ -3,6 +3,16 @@
 ########
 
 # Data From Existing Infrastructure
+data "terraform_remote_state" "existing-hub" {
+  backend = "azurerm"
+
+  config = {
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = "hub-net"
+    access_key           = var.access_key
+  }
+}
 
 data "terraform_remote_state" "existing-lz" {
   backend = "azurerm"
