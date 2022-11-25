40bfde979a8197ededa808e1c8ad60526c1ad738
David Cintron <dacintro@microsoft.com>
2022-11-24 20:58:46 -0600

    remove auth provider variables

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/04-Network-Hub/variables.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/04-Network-Hub/variables.tf
@@ -52,26 +52,3 @@ variable "storage_account_name" {
 variable "container_name" {
   default = "akscs"
 }
-
-
-# required in some cases
-
-# variable "subscription_id" {
-#   description = "Azure subscription Id."
-#   default     = null
-# }
-
-# variable "tenant_id" {
-#   description = "Azure tenant Id."
-#   default     = null
-# }
-
-# variable "client_id" {
-#   description = "Azure service principal application Id"
-#   default     = null
-# }
-
-# variable "client_secret" {
-#   description = "Azure service principal application Secret"
-#   default     = null
-# }
