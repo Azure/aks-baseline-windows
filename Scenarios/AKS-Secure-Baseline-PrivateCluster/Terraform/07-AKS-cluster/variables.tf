e2f82bda567f5505c4c1e84ee1622b4ef399765e
David Cintron <dacintro@microsoft.com>
2022-11-24 21:02:18 -0600

    set wnp to true

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/07-AKS-cluster/variables.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/07-AKS-cluster/variables.tf
@@ -12,7 +12,7 @@ variable "private_dns_zone_name" {
 
 variable "wnp_count" {
   type    = bool
-  default = false
+  default = true
 }
 
 variable "resource_group_name" {
@@ -29,25 +29,3 @@ variable "access_key" {
   type      = string
   sensitive = true
 }
-
-# required in some cases
-
-# variable "subscription_id" {
-#   description = "Azure subscription Id."
-#   default = null
-# }
-
-# variable "tenant_id" {
-#   description = "Azure tenant Id."
-#    default = null
-# }
-
-# variable "client_id" {
-# description = "Azure service principal application Id"
-# default = null
-# }
-
-# variable "client_secret" {
-#   description = "Azure service principal application Secret"
-#   default = null
-# }
