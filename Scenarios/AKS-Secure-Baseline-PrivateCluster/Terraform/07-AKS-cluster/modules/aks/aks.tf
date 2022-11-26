bbe69c3f5fdfe022013287652d029a260cb36bd2
David Cintron <dacintro@microsoft.com>
2022-11-26 15:38:49 -0600

    add admin group

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/07-AKS-cluster/modules/aks/aks.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/07-AKS-cluster/modules/aks/aks.tf
@@ -48,7 +48,7 @@ resource "azurerm_kubernetes_cluster" "akscluster" {
 
   azure_active_directory_role_based_access_control {
     managed = true
-    admin_group_object_ids = [var.appdevs]
+    #admin_group_object_ids = var.appdevs
     azure_rbac_enabled = true
   }
 
