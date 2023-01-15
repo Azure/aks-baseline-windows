74b660787cf8ca0eabcf8797e0d0e22c26462db6
David Cintron <dacintro@microsoft.com>
2023-01-15 09:10:45 -0600

    create private link subnet in lz

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/05-Network-LZ/lz-networking.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/05-Network-LZ/lz-networking.tf
@@ -20,9 +20,18 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.lz_prefix}-vnet"
   resource_group_name = azurerm_resource_group.spoke-rg.name
   location            = azurerm_resource_group.spoke-rg.location
-  address_space       = ["10.1.0.0/16"]
+  address_space       = ["10.240.0.0/16"]
   dns_servers         = null
   tags                = var.tags
+
+}
+
+resource "azurerm_subnet" "priv-link" {
+  name                                           = "priv-subnet"
+  resource_group_name                            = azurerm_resource_group.spoke-rg.name
+  virtual_network_name                           = azurerm_virtual_network.vnet.name
+  address_prefixes                               = ["10.240.4.32/28"]
+  enforce_private_link_endpoint_network_policies = true
 
 }
 
@@ -32,6 +41,10 @@ output "lz_vnet_name" {
 
 output "lz_vnet_id" {
   value = azurerm_virtual_network.vnet.id
+}
+
+output "priv_subnet_id" {
+  value = azurerm_subnet.priv-link.id
 }
 
 # # Create Route Table for Landing Zone
@@ -46,7 +59,7 @@ resource "azurerm_route_table" "route_table" {
     name                   = "route_to_firewall"
     address_prefix         = "0.0.0.0/0"
     next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.0.1.4"
+    next_hop_in_ip_address = "10.200.0.4"
   }
 }
 
