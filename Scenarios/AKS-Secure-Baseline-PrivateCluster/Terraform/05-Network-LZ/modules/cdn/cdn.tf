01843218ed1efc391220c536551342879c7b9698
David Cintron <dacintro@microsoft.com>
2023-03-20 07:39:12 -0500

    add diag for cdn

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/05-Network-LZ/modules/cdn/cdn.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/05-Network-LZ/modules/cdn/cdn.tf
@@ -9,6 +9,29 @@ resource "azurerm_cdn_frontdoor_endpoint" "cdn-ep" {
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.cdn-fd.id
 }
 
+resource "azurerm_monitor_diagnostic_setting" "cdn" {
+  name               = "cdndiagnostics"
+  target_resource_id = azurerm_cdn_frontdoor_profile.cdn-fd.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  enabled_log {
+    category_group = "allLogs"
+
+    retention_policy {
+      enabled = true
+      days = "30"
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = true
+      days = "30"
+    }
+  }
+}
   ####
   #### Input variables
   ####
@@ -22,5 +45,9 @@ variable "rg" {
 }
 
 variable "prefix" {
+  
+}
+
+variable "log_analytics_workspace_id" {
   
 }
