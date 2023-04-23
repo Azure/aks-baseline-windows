f8a097c05cd27afa9b1c96a6d5b1b6bbb86c69c8
Jose Luis Carrillo Aguilar <jose.carrillo@microsoft.com>
2023-04-23 19:49:15 +0200

    Adding naming examples to the test

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/00-Naming-module/test/main.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/00-Naming-module/test/main.tf
@@ -7,8 +7,16 @@ module "CAFResourceNames" {
   instance    = "001"
 }
 
-output "azurerm_key_vault" {
-  value = module.CAFResourceNames.names.azurerm_key_vault
+output "azurerm_route_table" {
+  value = module.CAFResourceNames.names.azurerm_route_table
+}
+
+output "azurerm_example1" {
+  value = module.CAFResourceNames.names.azurerm_example1
+}
+
+output "azurerm_example2" {
+  value = module.CAFResourceNames.names.azurerm_example2
 }
 
 output "all_resources" {
