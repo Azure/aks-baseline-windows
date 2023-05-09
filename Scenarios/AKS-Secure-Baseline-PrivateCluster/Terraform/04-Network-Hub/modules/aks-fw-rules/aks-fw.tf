fc2ff52f523df8477b1d680639acaf7926f1f592
dcint <41127763+dcint@users.noreply.github.com>
2023-05-08 21:40:05 -0500

    update  fw rule collections

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/04-Network-Hub/modules/aks-fw-rules/aks-fw.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/04-Network-Hub/modules/aks-fw-rules/aks-fw.tf
@@ -16,9 +16,10 @@ resource "azurerm_firewall_policy_rule_collection_group" "AKS" {
   name               = var.caf_basename.azurerm_firewall_policy_rule_collection_group
   firewall_policy_id = azurerm_firewall_policy.aks.id
   priority           = 200
+
   application_rule_collection {
-    name     = "aks_app_rules"
-    priority = 205
+    name     = "aks_app_global_rules"
+    priority = 200
     action   = "Allow"
     rule {
       name = "aks_service"
@@ -26,17 +27,43 @@ resource "azurerm_firewall_policy_rule_collection_group" "AKS" {
         type = "Https"
         port = 443
       }
-      source_addresses      = ["10.240.0.0/16"]
-      destination_fqdn_tags = ["AzureKubnernetesService"]
+      source_ip_groups      = [var.lx_ip_group, var.win_ip_group]
+      destination_fqdn_tags = [
+        "mcr.microsoft.com",
+        "*.data.mcr.microsoft.com",
+        "management.azure.com",
+        "login.microsoftonline.com",
+        "packages.microsoft.com",
+        "acs-mirror.azureedge.net"
+        ]
     }
   }
 
   application_rule_collection {
-    name     = "app_rule_collection2"
-    priority = 206
+    name     = "aks_app_linux_node_rules"
+    priority = 201
     action   = "Allow"
     rule {
-      name = "app_rule_collection1_rule2"
+      name = "linux_node_pools"
+      protocols {
+        type = "Http"
+        port = 80
+      }
+      source_ip_groups = [var.lx_ip_group]
+      destination_fqdns = [
+        "security.ubuntu.com",
+        "azure.archive.ubuntu.com",
+        "changelogs.ubuntu.com"
+      ]
+    }
+  }
+
+    application_rule_collection {
+    name     = "aks_app_win_node_rules"
+    priority = 202
+    action   = "Allow"
+    rule {
+      name = "win_node_pools"
       protocols {
         type = "Http"
         port = 80
@@ -45,89 +72,69 @@ resource "azurerm_firewall_policy_rule_collection_group" "AKS" {
         type = "Https"
         port = 443
       }
-      source_addresses = ["10.240.0.0/16"]
+
+      source_ip_groups = [var.win_ip_group]
       destination_fqdns = [
-        "*.cdn.mscr.io",
-        "mcr.microsoft.com",
-        "*.data.mcr.microsoft.com",
-        "management.azure.com",
-        "login.microsoftonline.com",
-        "acs-mirror.azureedge.net",
-        "dc.services.visualstudio.com",
-        "*.opinsights.azure.com",
-        "*.oms.opinsights.azure.com",
-        "*.microsoftonline.com",
-        "*.monitoring.azure.com",
+        "onegetcdn.azureedge.net",
+        "go.microsoft.com",
+        "*.mp.microsoft.com",
+        "www.msftconnecttest.com",
+        "ctldl.windowsupdate"
       ]
     }
   }
 
   application_rule_collection {
-    name     = "app_rule_collection3"
-    priority = 207
+    name     = "monitor_containers_rule"
+    priority = 203
     action   = "Allow"
     rule {
-      name = "app_rule_collection1_rule3"
-      protocols {
-        type = "Http"
-        port = 80
-      }
+      name = "monitor_containers"
       protocols {
         type = "Https"
         port = 443
       }
-      source_addresses = ["10.240.0.0/16"]
+      source_ip_groups = [var.lx_ip_group, var.win_ip_group]
       destination_fqdns = [
-        "download.opensuse.org",
-        "security.ubuntu.com",
-        "ntp.ubuntu.com",
-        "packages.microsoft.com",
-        "snapcraft.io"
+        "dc.services.visualstudio.com",
+        "*.ods.opinsights.azure.com",
+        "*.oms.opinsights.azure.com",
+        "*.monitoring.azure.com"
+      ]
+    }
+  }
+
+  application_rule_collection {
+    name     = "azure_policy_rules"
+    priority = 204
+    action   = "Allow"
+    rule {
+      name = "azure_policy"
+      protocols {
+        type = "Https"
+        port = 443
+      }
+      source_ip_groups = [var.lx_ip_group, var.win_ip_group]
+      destination_fqdns = [
+        "data.policy.core.windows.net",
+        "store.policy.core.windows.net",
+        "dc.services.visualstudio.com"
       ]
     }
   }
 
   network_rule_collection {
-    name     = "aks_network_rules"
-    priority = 201
-    action   = "Allow"
-    rule {
-      name                  = "https"
-      protocols             = ["TCP"]
-      source_addresses      = ["10.240.0.0/16"]
-      destination_addresses = ["*"]
-      destination_ports     = ["443"]
-    }
-    rule {
-      name                  = "dns"
-      protocols             = ["UDP"]
-      source_addresses      = ["10.240.0.0/16"]
-      destination_addresses = ["*"]
-      destination_ports     = ["53"]
-    }
-    rule {
-      name                  = "time"
-      protocols             = ["UDP"]
-      source_addresses      = ["10.240.0.0/16"]
-      destination_addresses = ["*"]
-      destination_ports     = ["123"]
-    }
-    rule {
-      name                  = "tunnel_udp"
-      protocols             = ["UDP"]
-      source_addresses      = ["10.240.0.0/16"]
-      destination_addresses = ["*"]
-      destination_ports     = ["1194"]
-    }
-    rule {
-      name                  = "tunnel_tcp"
-      protocols             = ["TCP"]
-      source_addresses      = ["10.240.0.0/16"]
-      destination_addresses = ["*"]
-      destination_ports     = ["9000"]
+      name     = "net_monitor_containers_rule"
+      priority = 205
+      action   = "Allow"
+      rule {
+       name                  = "net_monitor_containers"
+       protocols             = ["TCP"]
+       source_ip_groups      = [var.lx_ip_group, var.win_ip_group]
+       destination_addresses = ["AzureMonitor:443"]
+       destination_ports     = ["443"]
     }
   }
-
 }
 
 ##########################################################
@@ -135,7 +142,8 @@ resource "azurerm_firewall_policy_rule_collection_group" "AKS" {
 ##########################################################
 
 variable "caf_basename" {}
-
 variable "resource_group_name" {}
 variable "location" {}
 variable "firewallName" {}
+variable "win_ip_group" {}
+variable "lx_ip_group" {}
