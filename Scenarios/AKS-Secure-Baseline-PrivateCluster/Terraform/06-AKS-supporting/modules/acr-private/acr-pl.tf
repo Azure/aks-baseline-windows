dfdf5e215b64fcf1692d63af1dfe8e1213756317
dcint <41127763+dcint@users.noreply.github.com>
2023-05-21 08:00:28 -0500

    add acr output

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/06-AKS-supporting/modules/acr-private/acr-pl.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/06-AKS-supporting/modules/acr-private/acr-pl.tf
@@ -32,6 +32,9 @@ output "acr_id" {
   value = azurerm_container_registry.acr.id
 }
 
+output "acr_name" {
+  value = azurerm_container_registry.acr.name  
+}
 output "custom_dns_configs" {
   value = azurerm_private_endpoint.acr-endpoint.custom_dns_configs
 }
